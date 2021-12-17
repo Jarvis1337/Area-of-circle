e8a169f2f3fa94a0b0d3ea6fcc476694d7298009
Your's Jarvis <77638593+Yours-Jarvis@users.noreply.github.com>
2021-12-17 22:50:22 +0530

    Area of circle ⭕

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**| Program of Area of Circle with is made with C++ Language 🙃|**/
+
+/** 🚀 Please follow me on GitHub to stay tuned with us for more Exciting future
+ * Updates like this. | © 2021 — Made By ~Alpesh | Your's Jarvis #2431 with ♥
+ * **/
+
+#include <iostream>
+
+int main() {
+	float radius, area;
+
+	std::cout << "Enter the radius of circle : => | ";
+	std::cout << "";
+	std::cin >> radius;
+	area = 3.14 * radius * radius;
+	std::cout << "Area of circle is with the radius " << radius << " is |=> "
+			  << area;
+}
+
+/**| © 2021 — Coded By ~Alpesh | Your's Jarvis... |**/
+
+/**| © Do not share code without is credit's 🙃 |**/
